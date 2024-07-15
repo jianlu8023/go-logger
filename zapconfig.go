b24ef393ecbc2ac3d6b048127e11a01823870e2d
jianlu <jianlu8023@gmail.com>
2024-07-15 13:35:06 +0800

    添加zapcore的LevelEncoder

--- zapconfig.go
+++ zapconfig.go
@@ -6,6 +6,8 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
+const format = "2006-01-02 15:04:05.000"
+
 var consoleEncoderConfig = zapcore.EncoderConfig{
 	MessageKey:     "msg",
 	LevelKey:       "level",
@@ -14,7 +16,7 @@ var consoleEncoderConfig = zapcore.EncoderConfig{
 	StacktraceKey:  "stacktrace",
 	LineEnding:     zapcore.DefaultLineEnding,
 	EncodeLevel:    zapcore.CapitalColorLevelEncoder,
-	EncodeTime:     EncodeTime,
+	EncodeTime:     CustomTimeEncoder,
 	EncodeDuration: zapcore.SecondsDurationEncoder,
 	EncodeCaller:   zapcore.ShortCallerEncoder,
 	FunctionKey:    "func",
@@ -27,13 +29,17 @@ var fileEncoderConfig = zapcore.EncoderConfig{
 	StacktraceKey:  "stacktrace",
 	LineEnding:     zapcore.DefaultLineEnding,
 	EncodeLevel:    zapcore.CapitalLevelEncoder,
-	EncodeTime:     EncodeTime,
+	EncodeTime:     CustomTimeEncoder,
 	EncodeDuration: zapcore.SecondsDurationEncoder,
 	EncodeCaller:   zapcore.ShortCallerEncoder,
 	FunctionKey:    "func",
 	EncodeName:     zapcore.FullNameEncoder,
 }
 
-func EncodeTime(time time.Time, encoder zapcore.PrimitiveArrayEncoder) {
-	encoder.AppendString(time.Format("2006-01-02 15:04:05.000"))
+func CustomTimeEncoder(time time.Time, encoder zapcore.PrimitiveArrayEncoder) {
+	encoder.AppendString(time.Format(format))
+}
+
+func CustomCapitalStringLevelEncoder(level zapcore.Level, enc zapcore.PrimitiveArrayEncoder) {
+	enc.AppendString("[" + level.CapitalString() + "]")
 }
